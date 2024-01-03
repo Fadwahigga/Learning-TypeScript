d6df662b235ab6ecf0b4d6adfdb08d289ef4ddff
Fadwa Ali <fadwa.ali@elevoro.com>
2024-01-03 13:42:12 +0300

    Type Alias Advanced

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,9 @@ let mAraay: (number | boolean | string[] | number[])[] = [
   [1, 2, 3],
 ];
 /**Type Alias */
-type stringnumber = string | number;
-let test: stringnumber = 10;
-test = "fadwa";
+// type stringnumber = string | number;
+// let test: stringnumber = 10;
+// test = "fadwa";
 /** Type Alias Advanced */
 type Buttons = {
   u: string;
@@ -30,10 +30,11 @@ type Buttons = {
   r: string;
   d: string;
 };
-function getArr(btn: Buttons) {
+type Last = Buttons & { x: boolean };
+function getArr(btn: Last) {
   console.log(`UP ${btn.u}`);
   console.log(`LEFT ${btn.l}`);
   console.log(`RIGHT ${btn.r}`);
   console.log(`DOW ${btn.d}`);
-};
-getArr({u:"stt",l:"lee",r:"ree",d:"ssss"});
+}
+getArr({ u: "stt", l: "lee", r: "ree", d: "ssss", x: false });
